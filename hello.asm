ca390a49a1b37709b9089050b29d440f15371ed8
cygx <cygx@cpan.org>
2016-12-01 15:29:54 +0100

    make simple calls work

--- hello.asm
+++ hello.asm
@@ -1,13 +1,16 @@
 # hello world example code
 
 .hll asm
-
 .frame main
-.local int obj
-return_i %0
 
 .frame hello
 .local str
 const_s %0 'hello world'
-print %0
+say %0
 return
+
+.frame main
+.local int obj
+getcode %1 &hello
+.call %1
+return_i %0
