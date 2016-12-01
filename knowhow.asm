60ca67c2c553b917b53cd2b3de4e508ac30a1172
cygx <cygx@cpan.org>
2016-12-01 22:27:35 +0100

    remove backslash sugar: let's not go crazy

--- knowhow.asm
+++ knowhow.asm
@@ -8,9 +8,9 @@ exit %0
 .local[str] name repr
 checkarity 1 1
 param_rp_s @name 0
-const_s @repr \KnowHOWREPR
+const_s @repr 'KnowHOWREPR'
 knowhow @how
-findmeth @method @how \new_type
+findmeth @method @how 'new_type'
 .flags obj :str :str
 .result @rv
 .call @method @how name: @name repr: @repr
@@ -19,6 +19,6 @@ return_o @rv
 .frame load
 .set load
 .local str obj
-const_s %0 \create_knowhow_type
+const_s %0 'create_knowhow_type'
 getcode %1 &create_knowhow_type
 bindcurhllsym %1 %0 %1
